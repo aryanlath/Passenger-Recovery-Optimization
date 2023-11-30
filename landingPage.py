db87919b1dcfa7550d2804a64b7ec45bb3313096
AyushSavar <ayush.savar1@gmail.com>
2023-11-30 17:03:40 +0530

    Added comments to streamlit files

--- landingPage.py
+++ landingPage.py
@@ -1,28 +1,42 @@
 import streamlit as st
 #import streamlit library
 
+#add title
 st.title("Business Rules Modification Engine")
 st.title("Default Level Solution")
+
+#for spacing
 st.write("")
+
+#heading
 st.write("Business class passengers may be reallocated to-")
 
 
+#create two columns to align checkboxes
 col1,col2=st.columns(2)
-with col1:
+with col1: 
+    #variable value 1 if business to first class change allowed, 0 otherwise
     business_to_first=st.checkbox("First Class",key="1")    
 with col2:
+    #variable value 1 if business to economy class change allowed, 0 otherwise
     business_to_economy=st.checkbox("Economy Class",key="2")    
     
+#add empty spacing
 st.write("")
 st.write("")  
     
+
 st.write("First class passengers may be reallocated to-")
+#create two columns to align checkboxes
 col1,col2=st.columns(2)
 with col1:
+    #variable value 1 if first to business class change allowed, 0 otherwise
     first_to_business=st.checkbox("Business Class",key="3")    
 with col2:
+    #variable value 1 if first to economy class change allowed, 0 otherwise
     first_to_economy=st.checkbox("Economy Class",key="4")    
     
+#add empty spacing
 st.write("")
 st.write("")
 
@@ -30,15 +44,21 @@ st.write("Economy class passengers may be reallocated to-")
 
 col1,col2=st.columns(2)
 with col1:
+    #variable value 1 if economy to first class change allowed, 0 otherwise
     economy_to_first=st.checkbox("First Class",key="5")    
 with col2:
+    #variable value 1 if economy to business class change allowed, 0 otherwise
     economy_to_business=st.checkbox("Business Class",key="6")    
     
+    
+#add empty space
 st.write("")    
 st.write("")
 
+#variable value denotes maximum departure delay allowed with default value 20
 maxDepartDelay=st.number_input("Maximum departure delay",value=20,min_value=0)
 
+#columns made to bring button to center
 col1, col2, col3,col4,col5 = st.columns(5)
 with col1:
     pass
@@ -50,14 +70,25 @@ with col5 :
     pass
 with col3:
     if (st.button("Proceed")):
-        #write all variables to file when button is pressed
+        #when button is pressed
         f=open("default.py","w")
+        #overwrites file each time its run
+        
+        #writes class change permissions for business class
         f.write("business_to_first="+str(business_to_first)+"\n")
         f.write("business_to_economy="+str(business_to_economy)+"\n")
+        
+        #writes class change permissions for first class
         f.write("first_to_economy="+str(first_to_economy)+"\n")
         f.write("first_to_business="+str(first_to_business)+"\n")
+        
+        #writes class change permissions for economy class
         f.write("economy_to_business="+str(economy_to_business)+"\n")
         f.write("economy_to_first="+str(economy_to_first)+"\n")
+        
         f.write("maxDepartDelay="+str(maxDepartDelay)+"\n")
+        #write variable nameed maxDepartDelay 
+        
         f.close()
+        #closes file
         
--- pages/flightScore.py
+++ pages/flightScore.py
@@ -1,56 +1,100 @@
 import streamlit as st
+#import library 
 
+#write page title
 st.title("Flight Scores")
+
+#add empty space
 st.write("")
 
+#add heading
 st.subheader("Arrival Delay")
+
+#add columns for proper alignment 
 col1,col2=st.columns(2)
 with col1:
+    #score for arrival delay of less than 6 hours
     arrDelay6h=st.number_input("Less than 6 hours",value=70,min_value=0)
+    
+    #score for arrival delay of less than 12 hours
     arrDelay12h=st.number_input("Less than 12 hours",value=50,min_value=0)
 with col2:
+    #score for arrival delay of less than 24 hours
     arrDelay24h=st.number_input("Less than 24 hours",value=40,min_value=0)
+    
+    #score for arrival delay of less than 48 hours
     arrDelay48h=st.number_input("Less than 48 hours",value=30,min_value=0)
 
+#heading for next section
 st.subheader("STD of proposed flight")
+
+#divide into columns
 col1,col2=st.columns(2)
 with col1:
+    #score for departure delay of less than 6 hours
     STD6h=st.number_input("Less than 6 hours",value=70,min_value=0,key=1)
+   
+    #score for departure delay of less than 12 hours
     STD12h=st.number_input("Less than 12 hours",value=50,min_value=0,key=2)
 with col2:
+    #score for departure delay of less than 24 hours
     STD24h=st.number_input("Less than 24 hours",value=40,min_value=0,key=3)
+    
+    #score for departure delay of less than 48 hours
     STD48h=st.number_input("Less than 48 hours",value=30,min_value=0,key=4)
 
-
+#score for citypairs
 st.subheader("City Pairs")
+
+#divide into columns for alignment 
 col1,col2,col3=st.columns(3)
 with col1:
+    #score when citypairs same
     citypairsSame=st.number_input("Same citypairs",value=40,min_value=0)
+    
 with col2:
+    #score when citypairs different in same city
     citypairsSameCity=st.number_input("Different citypairs in same city",value=30,min_value=0)
+    
 with col3:
+    #score when citypairs different
     citypairsDifferent=st.number_input("Different citypairs",value=20,min_value=0)
 
-
+#cutoff for each grade
 st.subheader("Quality Score Grade")
+
 col1,col2,col3=st.columns(3)
 with col1:
+    #flight score for grade A
     gradeA=st.number_input("Minimum score for grade A",value=200,min_value=0)
+    
 with col2:
+    #flight score for grade B
     gradeB=st.number_input("Minimum score for grade B",value=180,min_value=0)
+    
 with col3:
+    #flight score for grade C
     gradeC=st.number_input("Minimum score for grade C",value=150,min_value=0)
 
+#other modifications
 st.subheader("Others")
+
+#divide into 2 columns
 col1,col2=st.columns(2)
 with col1:
+    #score when stopover is present
     stopOver=st.number_input("Stopover",value=-20)
+    
 with col2:
+    #score to add when equipment number matches
     equipmentScore=st.number_input("Matching Equipment number",value=50)
 
+
+#add empty space
 st.write("")
 st.write("")
 
+#columns created to bring button to center
 col1, col2, col3,col4,col5 = st.columns(5)
 with col1:
     pass
@@ -62,22 +106,36 @@ with col5 :
     pass
 with col3:
     if (st.button("Proceed")):
-        #writing all variables to file when button is pressed
+        #writing to file
         f=open("flightScores.py","w")
+        
+        #adding all arrival delay scores
         f.write("arrDelay6h="+str(arrDelay6h)+"\n")
         f.write("arrDelay12h="+str(arrDelay12h)+"\n")
         f.write("arrDelay24h="+str(arrDelay24h)+"\n")
         f.write("arrDelay48h="+str(arrDelay48h)+"\n")
+        
+        #adding all departure time scores
         f.write("STD6h="+str(STD6h)+"\n")
         f.write("STD12h="+str(STD12h)+"\n")
         f.write("STD24h="+str(STD24h)+"\n")
         f.write("STD48h="+str(STD48h)+"\n")
+        
+        #adding citypair scors
         f.write("citypairsSame="+str(citypairsSame)+"\n")
         f.write("citypairsSameCity="+str(citypairsSameCity)+"\n")
         f.write("citypairsDifferent="+str(citypairsDifferent)+"\n")
+        
+        #adding flight grade scores
         f.write("gradeA="+str(gradeA)+"\n")
         f.write("gradeB="+str(gradeB)+"\n")
         f.write("gradeC="+str(gradeC)+"\n")
+        
+        #adding score for stopover
         f.write("stopOver="+str(stopOver)+"\n")
+        
+        #adding score for equipment match
         f.write("equipmentScore="+str(equipmentScore)+"\n")
+        
+        #closing file
         f.close()
