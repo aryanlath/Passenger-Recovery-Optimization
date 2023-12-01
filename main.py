3ed2fb7c40f45ff7e1b9d61f9599dcdd371da0db
dhaneshragu <dhanesh.v@iitg.ac.in>
2023-12-01 13:27:50 +0530

    major changes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from feasible_flights import *
-from constants import *
+# from feasible_flights import *
+# from constants import *
 
-graph=create_flight_graph()
-# PNR_to_FLight_Object=init_PNR_to_Flight_Object()
+# graph=create_flight_graph()
+# # PNR_to_FLight_Object=init_PNR_to_Flight_Object()
 
-all_flights,pnr_objects,pnr_flight_mapping,pnr_to_s2 = Get_All_Maps()
-init_normalize_factors(all_flights,pnr_objects,pnr_flight_mapping,pnr_to_s2)
+# all_flights,pnr_objects,pnr_flight_mapping,pnr_to_s2 = Get_All_Maps()
+# init_normalize_factors(all_flights,pnr_objects,pnr_flight_mapping,pnr_to_s2)
